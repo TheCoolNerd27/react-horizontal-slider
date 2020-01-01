e323d2ff5c91421d51d31aa0201945e91ccc2470
TheCoolNerd27 <rohan.ashra27@gmail.com>
2020-01-01 22:20:04 +0530

    Project website bugs addressed

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import HorizontalMenu2 from './Components/Horizontal-menu2';
 function App() {
   return (
     <div className="App">
+      
         <HorizontalMenu2 title="Recently Viewed" data={items}width="300" height="300" id="1" />
         <HorizontalMenu2 title="Recently Viewed" data={items} id="2"  />
         <HorizontalMenu2 title="Recently Viewed" data={items} id="3" />
